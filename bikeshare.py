c2ceff6994167ee337ed7a578aa709f7e9c033d9
ZhangZhihong <zhihong.zhang@bmw-brilliance.cn>
2022-04-05 11:33:16 +0800

    Add comment for "get filters()" function

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#This function is for get what user want to see for the later data
 def get_filters():
 
     print('Hello! Let\'s explore some US bikeshare data!')
